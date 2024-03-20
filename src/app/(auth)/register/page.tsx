a363f1db4f63329a6ba5a3ed67eba2ebe3176226
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-03-20 13:13:10 +0700

    feat: login page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
+
+const RegisterPage = () => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | CMS App",
+    default: "CMS App",
+  },
+  description: "The official CMS app build by Arief Rachman Hakim",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
